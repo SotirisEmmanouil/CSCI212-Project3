4e2fda2669044482e36995ae87ab0b0ef5d39da9
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-01-15 19:46:39 -0500

    Update Project3.java

--- Project3.java
+++ Project3.java
@@ -1,7 +1,6 @@
 public class Project3 {
   
 public static void main (String [] args) {
-	  		DateGUI Date = new DateGUI();		    //main
-	  	
-      }	
+	  DateGUI Date = new DateGUI();		    //main
+	  	   }	
 	}
